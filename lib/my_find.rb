ec45a62116e377b5930e7e0be5837c15ab074dca
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-26 18:02:03 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,12 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-  yield collection[i]
+  return collection[i] if yield(collection[i])
   i += 1 
   end 
 end
 
 collection = (1..100).to_a 
+my_find(collection) do |i|
+  (i % 3 == 0) && (i % 5 == 0)
+end
